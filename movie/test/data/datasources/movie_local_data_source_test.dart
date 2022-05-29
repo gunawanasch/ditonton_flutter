c98a6181704c5046577bf5b3fa380f2e15447018
gunawanaschari <gunawan.aschari@gmail.com>
2022-05-30 01:57:46 +0700

    Update test

--- movie/test/data/datasources/movie_local_data_source_test.dart
+++ movie/test/data/datasources/movie_local_data_source_test.dart
@@ -1,7 +1,7 @@
 import 'package:core/utils/exception.dart';
-import '../../../../movie/lib/data/datasources/movie_local_data_source.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:movie/data/datasources/movie_local_data_source.dart';
 
 import '../../dummy_data/dummy_objects.dart';
 import '../../helpers/test_helper.mocks.dart';
